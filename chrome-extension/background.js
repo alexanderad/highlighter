21d6de9ec0aa878420a3b1f32098b0d4b889346e
Alexander Shchapov <sasha@imedicare.com>
2017-05-01 19:13:47 +0300

    Chrome extensions: add context menu support.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,20 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+function openHighlighterTab(url, set_active) {
     chrome.tabs.create({
-        "url": "https://highlighter.darednaxella.name/parse?u=" + encodeURI(tab.url)
+        "url": "https://highlighter.darednaxella.name/parse?u=" + encodeURI(url),
+        "active": set_active
     });
+}
+
+chrome.contextMenus.create({
+    "id": "id-open-link-in-highlighter",
+    "title": "Send to Highlighter",
+    "contexts": ["link"]
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    openHighlighterTab(tab.url, true);
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    openHighlighterTab(info.linkUrl, false);
 });
