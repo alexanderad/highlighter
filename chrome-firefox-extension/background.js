48fa6a1fef889cc4bc5d133a239aca5ae0d86721
Alexander Shchapov <sasha@imedicare.com>
2017-05-20 22:39:58 +0300

    Addon: add Firefox version.

---
+++ chrome-firefox-extension/background.js
@@ -0,0 +1,34 @@
+function getRootNamespace() {
+    var isChrome = !!window.chrome;
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+
+    if (isChrome) {
+        return chrome;
+    }
+    if (isFirefox) {
+        return browser;
+    }
+}
+
+var rootNamespace = getRootNamespace();
+
+function openHighlighterTab(url, set_active) {
+    rootNamespace.tabs.create({
+        "url": "https://highlighter.darednaxella.name/parse?u=" + encodeURI(url),
+        "active": set_active
+    });
+}
+
+rootNamespace.contextMenus.create({
+    "id": "id-open-link-in-highlighter",
+    "title": "Send to Highlighter",
+    "contexts": ["link"]
+});
+
+rootNamespace.browserAction.onClicked.addListener(function(tab) {
+    openHighlighterTab(tab.url, true);
+});
+
+rootNamespace.contextMenus.onClicked.addListener(function(info, tab) {
+    openHighlighterTab(info.linkUrl, false);
+});
