634a207662ecdff20e80d19ba48d66def1ac9a56
Alexander Shchapov <sasha@imedicare.com>
2019-07-20 13:03:57 +0200

    Bump extension version.

--- chrome-firefox-extension/newtab/scripts.js
+++ chrome-firefox-extension/newtab/scripts.js
@@ -1,6 +1,6 @@
 (function() {
   function fetchWord() {
-    fetch("http://highlighter.alpaca.engineering/v1/words/random")
+    fetch("https://highlighter.alpaca.engineering/v1/words/random")
       .then(response => response.json())
       .then(json => {
         drawWord(json["data"]);
